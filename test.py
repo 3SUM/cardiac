96156dcd9f4f570809b98e25c9754f4c756d90b3
Luis Maya Aranda <mayafluis@gmail.com>
2021-03-09 01:17:38 -0800

    Model works now alone

---
+++ test.py
@@ -0,0 +1,33 @@
+import numpy as np
+import joblib
+
+vectorizer = joblib.load("vectorizer.joblib")
+model = joblib.load("model.joblib")
+
+
+def _get_profane_prob(prob):
+    return prob[1]
+
+
+def predict(texts):
+    return model.predict(vectorizer.transform(texts))
+
+
+def predict_prob(texts):
+    return np.apply_along_axis(
+        _get_profane_prob, 1, model.predict_proba(vectorizer.transform(texts))
+    )
+
+
+if __name__ == "__main__":
+    texts = [
+        "Hello there, how are you",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        "!!!! Click this now!!! -> https://example.com",
+        "fuck you",
+        "fUcK u",
+        "GO TO hElL, you dirty scum",
+    ]
+
+    probs = predict_prob(texts)
+    print(probs)
--- train.py
+++ train.py
@@ -1,23 +1,19 @@
-import joblib
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.calibration import CalibratedClassifierCV
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
+from sklearn.calibration import CalibratedClassifierCV
+from joblib import dump
 
-# Read data
 data = pd.read_csv("data/data.csv")
 texts = data["text"].astype(str)
 y = data["is_offensive"]
 
-# Vectorize the text
-vectorizer = CountVectorizer(stop_words="english", min_df=0.0001)
+vectorizer = TfidfVectorizer(stop_words="english", min_df=0.0001)
 X = vectorizer.fit_transform(texts)
 
-# Train the model
 model = LinearSVC(class_weight="balanced", dual=False, tol=1e-2, max_iter=1e5)
 cclf = CalibratedClassifierCV(base_estimator=model)
 cclf.fit(X, y)
 
-# Save the model
-joblib.dump(vectorizer, "vectorizer.joblib")
-joblib.dump(cclf, "model.joblib")
+dump(vectorizer, "vectorizer.joblib")
+dump(cclf, "model.joblib")
